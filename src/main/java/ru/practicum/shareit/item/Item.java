7423abecd113cb5ab334b53c8d48aaced9578f6c
alekseytumbaev <leha.tumbaev@gmail.com>
2023-05-27 09:42:53 +0700

    feat: - Connection to postgres (properties and data-jpa) - javax.persistence annotations for Item and User

--- src/main/java/ru/practicum/shareit/item/Item.java
+++ src/main/java/ru/practicum/shareit/item/Item.java
@@ -2,12 +2,24 @@ package ru.practicum.shareit.item;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "items")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "item_id")
     private long id;
+    
+    @Column(name = "item_name")
     private String name;
+
     private String description;
     private Boolean available;
     private long ownerId;
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -2,11 +2,24 @@ package ru.practicum.shareit.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
     private long id;
+
+    @Column(name = "user_name")
     private String name;
+
+    @Column(unique = true)
     private String email;
 }
